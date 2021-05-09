1033f06b816c35cdbaedb2cccf3a8841aac4af3a
Alexander Robinson <robinson@ucm.es>
2021-05-09 17:48:57 +0200

    Organization of ismip6 module.

--- ismip6.f90
+++ ismip6.f90
@@ -2,8 +2,20 @@ module ismip6
     ! This module contains routines that help with performing the ISMIP6 suite
     ! of experiments. 
     
+    use varslice 
+
     implicit none 
 
+    ! Class for holding ice-forcing data from ISMIP6 archives
+    type ismip6_forcing_class
+
+
+    end type
+
+    ! Class for holding ice output for writing to standard formats...
+    type ismip6_ice_class
+
+    end type 
 
 contains
 
